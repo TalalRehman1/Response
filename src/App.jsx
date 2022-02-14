3e368fd5e19ecb3ee8b8460c8391a4a0e7a1d646
talalrehman1 <m.talal9989@gmail.com>
2022-02-14 13:15:27 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Img from "./images/images-7.jpg"
+function App() {
+  return (
+    <div className="container  border  border-primary align-items-center" >
+<div className="  row border border-danger p-4 ">
+    <h1>Publicity offers to the media</h1>
+<div className='col-lg-5 col-md-4p-4'>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, sed aut consectetur nobis ipsam, provident libero repellat blanditiis eum ipsum possimus molestias quas nihil pariatur, deleniti enim voluptatum. Possimus, impedit.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/><br/>  Accusamus quisquam non neque doloremque itaque nesciunt modi accusantium, similique odit deleniti laudantium, perferendis a commodi! Excepturi quibusdam non porro optio consequuntur.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam reiciendis facere iusto rerum optio, itaque ex distinctio! Quaerat saepe recusandae voluptates officia fugiat! Vero, expedita et. Corrupti deleniti et quo!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic beatae<br/><br/> laudantium eos maxime tempora nam repellat earum vitae repellendus porro cupiditate reiciendis, ad deserunt minus dolorum assumenda nemo voluptate sequi!
+    
+
+
+</div>
+<div className='col-lg-7 col-md-12 d-flex justify-content-center p-4'> 
+<img src={Img} class="img-fluid" alt="..."></img></div>
+</div>
+    </div>
+  )
+}
+
+export default App
